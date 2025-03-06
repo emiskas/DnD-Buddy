561c2d376298a6354cbe18a9f71b744b751ab015
emiskas <emilispaulauskas279@gmail.com>
2025-03-06 19:15:26 +0200

    Simplified and added a prefix (!) for calling commands.

--- responses.py
+++ responses.py
@@ -1,17 +1,12 @@
 from random import choice, randint
 
-from dice import dice
-
 #place for chat gpt later
 
 def get_response(user_input : str) -> str:
     lowered: str = user_input.lower()
-    
-    if lowered == '':
-        return 'Well, you\'re awfully silent..'
-    elif 'hello' in lowered:
-        return 'Hello fleshling'
-    elif lowered in dice:
-        return f'you rolled: {randint(1, int(lowered[1:]))}'
-    else:
-        return choice(['da hell O _ o', 'uhh whut', 'mhm mhm(i don\'t get it)'])
+
+    if lowered.startswith("!"):
+        if lowered[1:] in ("d4", "d6", "d8", "d10", "d12", "d20"):
+            return f'you rolled: {randint(1, int(lowered[2:]))}'
+        else:
+            return choice(['da hell O _ o', 'uhh whut', 'mhm mhm(i don\'t get it)'])
